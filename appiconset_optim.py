b4c0acbaaaac59f0e97deab7f7fb32d2fa55abce
klaudz <klaudzliang@gmail.com>
2020-02-22 12:51:33 +0800

    refactor(AppIcons): Refactor naming

--- appiconset_optim.py
+++ appiconset_optim.py
@@ -5,28 +5,26 @@ import sys, os
 # pragma mark - Global Defines
 
 _script_dir = sys.path[0]
-_appicons_path = None
 
 # pragma mark - Main
 
 def main():
-    global _appicons_path
-    _appicons_path = obtain_appicons_path(sys.argv[1])
+    iconset_path = obtain_iconset_path(sys.argv[1])
 
 # pragma mark - Paths
     
-def obtain_appicons_path(path):
+def obtain_iconset_path(path):
     path = os.path.abspath(path)
     ext = os.path.splitext(path)[1].lower()
     if ext == '.appiconset':
-        appicons_path = path
+        iconset_path = path
     elif ext == '.xcassets':
         image_assets_path = path
-        appicons_name = 'AppIcon.appiconset'
-        appicons_path = os.path.join(image_assets_path, appicons_name)
+        iconset_name = 'AppIcon.appiconset'
+        iconset_path = os.path.join(image_assets_path, iconset_name)
     else:
-        appicons_path = None
-    return appicons_path
+        iconset_path = None
+    return iconset_path
 
 # pragma mark -
 
